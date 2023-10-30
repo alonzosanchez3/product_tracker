e3c5d341a81b215396de61906b48fe3d96fa6263
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-10-30 12:23:02 -0500

    added smtplib

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import smtplib
 
 headers = {
   "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
