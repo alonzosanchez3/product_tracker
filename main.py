d1dd5c76bb7bdb6d113bd85ded7085cc12b46bd9
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-11-04 12:23:02 -0500

    set up smtp and sending email

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ headers = {
   "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
   "Accept-Language": 'en-US,en;q=0.9'
 }
+url = 'https://www.amazon.com/LEXiBOOK-Miraculous-Educational-Bilingual-Activities/dp/B0B6HKB3JF/ref=sr_1_1_sspa?crid=3II92RG1YA1N6&keywords=baby%2Blaptop&qid=1699846232&sprefix=baby%2Blaptop%2Caps%2C121&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1'
 
 def get_price():
 
@@ -34,6 +35,7 @@ print(price_as_float)
 
 if price_as_float < BUY_PRICE:
   message = f'The kids laptop is now {price}'
-  with smtplib.SMTP('alonzosanchez3@gmail.com', port=587) as connection:
+  with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
     connection.starttls()
-    result = connection.login('alonzosanchez.com', )
+    result = connection.login('alonzosanchez3@gmail.com', 'knui yyfd mqlt igcb')
+    connection.sendmail(from_addr='alonzosanchez3@gmail.com', to_addrs='alonzosanchez3@gmail.com', msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode('utf-8'))
